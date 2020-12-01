1cd99410170eb838b6831d5dac574024a66d0658
Mads <mads.rosenlund@hotmail.com>
2020-12-01 12:17:10 +0100

    Payment changes (not final)

--- src/com/kea/MobilePay.java
+++ src/com/kea/MobilePay.java
@@ -1,5 +1,7 @@
 package com.kea;
 
+import java.util.Scanner;
+
 public class MobilePay extends Payment {
 
 }
--- src/com/kea/Payment.java
+++ src/com/kea/Payment.java
@@ -1,9 +1,76 @@
 package com.kea;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 abstract public class Payment {
     protected int phoneNumber;
     protected int cardNumber;
+    Scanner scan = new Scanner(System.in);
+
+    public void runPay() throws IOException {
+        pay();
+        payMobilepay();
+        payCreditcard();
+    }
 
     protected void pay() {
+        System.out.println("Choose your payment method: ");
+        System.out.println("1 Pay with Creditcard");
+        System.out.println("2 Pay with Mobilepay");
+        int num = scan.nextInt();
+        if (num==1){
+            payCreditcard();
+        }
+        else if (num==2){
+            payMobilepay();
+        }
+        else {
+            System.out.println("Invalid input. Please try again.");
+            pay();
+        }
+
+    }
+
+    public void payCreditcard() {
+        String creditregex = "^\\d{16}$";
+        boolean creditboo = true;
+        Pattern p = Pattern.compile(creditregex);
+        Matcher m = p.matcher(creditregex);
+
+        do {
+            System.out.println("Enter a valid 16 diget creditcard number: ");
+            double number = scan.nextDouble();
+            if (m.matches()) {
+                System.out.println("X amount have been charged.");
+            } else {
+                System.out.println("Creditnumber is not valid. Please try again.");
+                payMobilepay();
+
+            }
+        } while (creditboo) ;
+
+    }
+
+    public void payMobilepay() {
+        String mobileregex = "^\\d{8}$";
+        boolean mobileboo = true;
+        Pattern p = Pattern.compile(mobileregex);
+        Matcher m = p.matcher(mobileregex);
+
+        do {
+            System.out.println("Enter a valid 8 diget phonenumber: ");
+            double number = scan.nextDouble();
+            if (m.matches()) {
+                System.out.println("X amount have been charged.");
+            } else {
+                System.out.println("Mobilenumber is not valid. Please try again.");
+                payMobilepay();
+
+            }
+        } while (mobileboo) ;
+
     }
 }
