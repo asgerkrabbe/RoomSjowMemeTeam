76475b08d971272bf66644767157de23d6d0be62
Ines <inesluparia@gmail.com>
2020-12-03 11:27:36 +0100

    update

--- src/com/kea/Session.java
+++ src/com/kea/Session.java
@@ -10,7 +10,7 @@ public class Session {
     StreamList streamList;
 
 
-    public Session(Profile profile,StreamList streamList) /*ArrayList<Stream> streams*/ {
+    public Session(Profile profile, StreamList streamList) /*ArrayList<Stream> streams*/ {
         this.profile = profile;
         this.streamList = streamList;
         //this.streams = streams;
@@ -28,6 +28,45 @@ public class Session {
         String genre = sc.nextLine();
         Genre enumGenre = Genre.valueOf(genre);
 
+        LocalDateTime startTime = promptDateTime();
+
+        Stream stream = new Stream(startTime, streamTitle, enumGenre);
+        if (checkOverlap(stream, profile.myStreams)) {
+            profile.myStreams.add(stream);
+            streamList.streams.add(stream);
+        } else {
+            System.out.println("You already signed up for a stream at the selected time, would you like to choose another time?");
+            System.out.println("1. Yes \n2. No");
+            String answer = sc.next();
+            switch (answer) {
+                case "1":
+                    LocalDateTime startTime2 = promptDateTime();
+                    Stream stream2 = new Stream(startTime2, streamTitle, enumGenre);
+                    if (checkOverlap(stream2, profile.myStreams))
+                        System.out.println("You are also signed up for a stream at the selected time. \n");
+                    System.out.println("You will be forwarded to the start menu. ");
+                        createStream();
+                case "2":
+                    sessionMenu();
+            }
+        }
+
+    }
+
+    private boolean checkOverlap(Stream stream, ArrayList<Stream> myStreams) {
+        LocalDateTime thisDateTime = stream.getStartTime().plusHours(2);
+        for (Stream s : myStreams){
+            LocalDateTime DateTime = s.getStartTime().plusHours(2);
+            if thisDateTime "er minus end" DateTime...
+
+        }
+
+
+        return false;
+    }
+
+    public LocalDateTime promptDateTime() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Type the stream date and start time in this format: YYYY MM DD HH MM");
         String date = sc.nextLine();
         Scanner stringSc = new Scanner(date);
@@ -36,20 +75,11 @@ public class Session {
         int day = stringSc.nextInt();
         int hours = stringSc.nextInt();
         int minutes = stringSc.nextInt();
-        LocalDateTime startTime = LocalDateTime.of(year,month,day,hours,minutes);
+        LocalDateTime startTime = LocalDateTime.of(year, month, day, hours, minutes);
         System.out.println(startTime);
         System.out.println("The start date chose is " + (DateTimeFormatter.ISO_LOCAL_DATE).format(startTime));
         System.out.println("The start time chose is " + (DateTimeFormatter.ISO_LOCAL_TIME).format(startTime));
-
-        Stream stream = new Stream(startTime,streamTitle,enumGenre);
-        profile.myStreams.add(stream);
-        streamList.streams.add(stream);
-
-
-        //LocalDateTime startTime = LocalDateTime.of(year,month,day,5,2);
-        //System.out.println(startTime);
-        //LocalDateTime localTimeObj = LocalDateTime.parse(time);
-
+        return startTime;
     }
 
     public void sessionMenu() {
