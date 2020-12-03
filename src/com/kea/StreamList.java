243fc7262bfcadba8c1378deae21451e9a43f0bc
asgerkrabbe <asager@gmail.com>
2020-12-03 15:36:01 +0100

    update

--- src/com/kea/StreamList.java
+++ src/com/kea/StreamList.java
@@ -8,39 +8,40 @@ import java.util.Scanner;
 
 public class StreamList {
     ArrayList<Stream> streams = new ArrayList<Stream>();
+    ArrayList<String> showStreams = new ArrayList<>();
+    File streamsFile = new File("Streams.txt");
+    Scanner fileSc = new Scanner(streamsFile);
 
 
     public StreamList() throws FileNotFoundException {
-        loadStreams();
-
+        //loadStreams();
     }
 
     /**
-     *
      * loadStreams method needs to be modified. New stream format consists of
      * 6 index' instead of the current 3. Check Streams.txt for examples
      */
 
-    private void loadStreams() throws FileNotFoundException {
+    private void loadStreams() {
         streams = new ArrayList<>();
-        File streamsFile = new File("Streams.txt");
-        Scanner fileSc = new Scanner(streamsFile);
-        while(fileSc.hasNext()) {
+        while (fileSc.hasNext()) {
             String[] splittedLine = fileSc.nextLine().split(",");
+
             Genre genreEnum = Genre.valueOf(splittedLine[3]);
-            Stream s = new Stream(splittedLine[0], LocalDateTime.parse(splittedLine[1]), genreEnum,0,5.0);
+            Stream s = new Stream(splittedLine[0], LocalDateTime.parse(splittedLine[1]), genreEnum, 0, 5.0);
         }
     }
-
-
-    public void add(Stream stream){
+    public void add(Stream stream) {
         streams.add(stream);
-     }
+    }
 
     public void showList() {
-        Collections.sort(streams);
-        for(int i=0; i<streams.size(); i++){
-            System.out.println(streams.get(i));
+        while (fileSc.hasNext()) {
+            showStreams.add(fileSc.nextLine());
+        }
+        for (int i = 0; i < showStreams.size(); i++) {
+            System.out.println(showStreams.get(i));
+            }
         }
     }
-}
+
