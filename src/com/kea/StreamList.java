69c0e0184234e7940c8fb2f0b5e193d5c8091a53
asgerkrabbe <asager@gmail.com>
2020-12-07 12:40:55 +0100

    LoadStream working

--- src/com/kea/StreamList.java
+++ src/com/kea/StreamList.java
@@ -18,8 +18,6 @@ public class StreamList {
     Scanner fileSc = new Scanner(streamsFile);
     Scanner sc = new Scanner(System.in);
 
-    private String currentUser;
-
     /**
      * Displays the list of streams
      * @throws FileNotFoundException required exception when working with files
@@ -33,9 +31,6 @@ public class StreamList {
      * Setter for currentUser
      * @param currentUser the user currently using the program
      */
-    public void setCurrentUser(String currentUser) {
-        this.currentUser = currentUser;
-    }
 
 
 
@@ -53,23 +48,21 @@ public class StreamList {
 
     public void loadStreams() {
         while (fileSc.hasNext()) {
-            showStreams.add(fileSc.nextLine());
-        }
-        while (fileSc.hasNext()) {
             String[] splittedLine = fileSc.nextLine().split(",");
 
             LocalDate date = LocalDate.parse(splittedLine[0]);
             LocalTime time = LocalTime.parse(splittedLine[1]);
             LocalDateTime dateTime = LocalDateTime.of(date,time);
+            String title = splittedLine[2];
             Genre genreEnum = Genre.valueOf(splittedLine[3]);
             int viewers = Integer.parseInt(splittedLine[4]);
             double price = Double.parseDouble(splittedLine[5]);
+
             Stream s = new Stream(dateTime, splittedLine[2], genreEnum,viewers,price);
             streams.add(s);
             for (Stream str : streams)
                 System.out.println(str);
         }
-
         //fileSc.close();
     }
 
