66a3e8722005601809d0aaffd64e73ebb094847f
asgerkrabbe <asager@gmail.com>
2020-12-10 12:56:52 +0100

    update

--- src/com/kea/Content.java
+++ src/com/kea/Content.java
@@ -19,7 +19,7 @@ public class Content extends JPanel {
         JLabel label = new JLabel( new ImageIcon("PBDog.gif") );
         add( label );
         JFrame frame = new JFrame("RoomSjow");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         frame.add(new com.kea.Content());
         frame.pack();
         frame.setVisible(true);
