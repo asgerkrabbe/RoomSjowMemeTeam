259ee14ef4ba7cc95f13bfe7435cf3e6fa65e6e9
asgerkrabbe <asager@gmail.com>
2020-12-11 11:31:17 +0100

    bug fixes

--- src/com/kea/Session.java
+++ src/com/kea/Session.java
@@ -329,8 +329,7 @@ public class Session {
         } else {
             String s = null;
             if (time >= -120 && time <= 0) {
-                System.out.println("Your stream is live! Please enjoy your content, press enter to watch the stream.");
-                //String begin = inputSc.nextLine();
+                System.out.println("Your stream is live! Please enjoy your content.");
 
                 Content content = new Content();
 
